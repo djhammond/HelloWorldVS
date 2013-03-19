9dde8bf16506f12be2dc1f028c5a74554478d822
djhammond <dennis.hammond@Mindbodyonline.com>
2013-03-19 15:27:59 -0700

    Finished Code
    Compeleted STE Homework #1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+            else
+            {
+                string name = args[0];
+                Console.WriteLine ("Hello, " + name);
+            }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
